eadf4b5c1159cdd51033a7d1d29bd81702e39851
caroline6927 <caroline6927@gmail.com>
2016-09-12 00:18:32 +0800

    Initial Commit

---
+++ QuickStartApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickStartApp
+//
+//  Created by Wang Yujia on 12/9/16.
+//  Copyright © 2016 National University of Singapore Design Centric Program. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ QuickStartAppTests/QuickStartAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  QuickStartAppTests.swift
+//  QuickStartAppTests
+//
+//  Created by Wang Yujia on 12/9/16.
+//  Copyright © 2016 National University of Singapore Design Centric Program. All rights reserved.
+//
+
+import XCTest
+@testable import QuickStartApp
+
+class QuickStartAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
