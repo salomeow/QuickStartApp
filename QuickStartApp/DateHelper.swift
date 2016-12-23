14449b1502695f6aab29938c0b9eca340072e9f1
caroline6927 <caroline6927@gmail.com>
2016-12-23 22:15:04 +0800

    update | date helper
    date helper function added

--- QuickStartApp/DateHelper.swift
+++ QuickStartApp/DateHelper.swift
@@ -2,76 +2,30 @@
 //  DateHelper.swift
 //  QuickStartApp
 //
-//  Created by Wang Yujia on 28/9/16.
+//  Created by Wang Yujia on 22/12/16.
 //  Copyright © 2016 National University of Singapore Design Centric Program. All rights reserved.
 //
 
 import Foundation
-//////////////////////////////////////////////////////////////////////////////////////////////
-//func > (left: NSDate, right: NSDate) -> Bool {
-//    return left.compare(right) == .OrderedDescending
-//}
-//
-//extension NSCalendar {
-//    func dateRange(startDate startDate: NSDate, endDate: NSDate, stepUnits: NSCalendarUnit, stepValue: Int) -> DateRange {
-//        let dateRange = DateRange(calendar: self, startDate: startDate, endDate: endDate,
-//                                  stepUnits: stepUnits, stepValue: stepValue, multiplier: 0)
-//        return dateRange
-//    }
-//}
-//
-//struct DateRange :SequenceType {
-//
-//    var calendar: NSCalendar
-//    var startDate: NSDate
-//    var endDate: NSDate
-//    var stepUnits: NSCalendarUnit
-//    var stepValue: Int
-//    private var multiplier: Int
-//
-//    func generate() -> Generator {
-//        return Generator(range: self)
-//    }
-//
-//    struct Generator: GeneratorType {
-//
-//        var range: DateRange
-//
-//        mutating func next() -> NSDate? {
-//            guard let nextDate = range.calendar.dateByAddingUnit(range.stepUnits,
-//                                                                 value: range.stepValue * range.multiplier,
-//                                                                 toDate: range.startDate,
-//                                                                 options: []) else {
-//                                                                    return nil
-//            }
-//            if nextDate > range.endDate {
-//                return nil
-//            }
-//            else {
-//                range.multiplier += 1
-//                return nextDate
-//            }
-//        }
-//    }
-//}
-//
 
-////////////////////////////////////////////////////////////////////////////////////////////
-//        // Usage:
-//        func testDateRange() -> DateRange {
-//
-//            let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
-//            let startDate = NSDate(timeIntervalSinceNow: -(24*60*60*10-1))
-//            let endDate = NSDate(timeIntervalSinceNow: 0)
-//            let dateRange = calendar.dateRange(startDate: startDate,
-//                                               endDate: endDate,
-//                                               stepUnits: .Day,
-//                                               stepValue: 1)
-//            return dateRange
-//        }
-//
-//        let dates = testDateRange()
-//        let dateFormatter = NSDateFormatter()
-//        dateFormatter.dateFormat = "yyyy-MM-dd"
-//        var download_date: String
-////////////////////////////////////////////////////////////////////////////////////////////
+class DateHelper
+{
+    static let sharedInstance = DateHelper()
+    func get_date_list(date_range_n: Int = 30) -> [String] {
+        let now = Date()
+        let myLocale = Locale(identifier: "sg_SG")
+        let formatter = DateFormatter()
+        formatter.locale = myLocale
+        formatter.dateFormat = "y-MM-dd"
+        var dateStr = formatter.string(from: now)
+        var date_list=[dateStr]
+        for i in 1...date_range_n {
+            let interval_count = TimeInterval(24*60*60*i)
+            let day = Date(timeIntervalSinceNow: -interval_count)
+            dateStr = formatter.string(from: day)
+            date_list.append(dateStr)
+        }
+        return date_list
+    }
+
+}
